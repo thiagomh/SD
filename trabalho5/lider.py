22df47b8b446c57d1083032e143de19a3e3db88e
Thiago Henrique <thiagomhenrique@gmail.com>
2025-07-03 14:45:36 -0300

    eh

--- trabalho5/lider.py
+++ trabalho5/lider.py
@@ -13,6 +13,10 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
         self.offset = 0
         self.enderecos_replicas = enderecos_replicas
         self.stubs: list[replicacao_pb2_grpc.ServidorServiceStub] = []
+        self.arquivo_log = "log_lider.txt"
+        self.arquivo_banco = "banco_lider.txt"
+
+        self.carrega_banco()
 
         for endereco in enderecos_replicas:
             canal = grpc.insecure_channel(endereco)
@@ -28,7 +32,7 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
                 else:
                     print(f"Replica recusou entrada: {ack.mensagem}")
             except Exception as e:
-                print(f"Erro ao replicar para uma réplica: {e}")
+                print(f"Erro ao replicar para uma replica: {e}")
 
     def EnviarDado(self, request, context):
         self.offset += 1 
@@ -38,13 +42,14 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
             conteudo=request.conteudo
         )
 
-        self.log.append({
+        entrada_dict = {
             'epoca': self.epoca,
             'offset': self.offset,
             'conteudo': request.conteudo
-        })
+        }
+        self.log.append(entrada_dict)
+        self.salvar_em_log(entrada_dict)
 
-        # Push
         acks = 0
         acked_stubs: list[replicacao_pb2_grpc.ServidorServiceStub] = []
         for stub in self.stubs:
@@ -74,6 +79,7 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
                 'offset': self.offset,
                 'conteudo': request.conteudo
             }
+            self.salvar_em_banco(self.banco[chave])
 
             print(f"[Líder] Entrada committed: {chave} → {request.conteudo}")
             return replicacao_pb2.Resposta(
@@ -81,9 +87,9 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
                 conteudo=chave
             )
         else:
-            print(f"[Líder] Falha no quórum. Apenas {acks} acks recebidos.")
+            print(f"[Líder] Falha no quorum. Apenas {acks} acks recebidos.")
             return replicacao_pb2.Resposta(
-                mensagem="Erro: não foi possível confirmar a entrada (quórum não atingido).",
+                mensagem="Erro: nao foi possível confirmar a entrada (quorum nao atingido).",
                 conteudo=""
             )
     
@@ -91,7 +97,7 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
         entrada = self.banco.get(request.chave)
         if entrada:
             return replicacao_pb2.Resposta(
-                mensagem=f"Dado encontrado na época {entrada['epoca']} com offset {entrada['offset']}.",
+                mensagem=f"Dado encontrado na epoca {entrada['epoca']} com offset {entrada['offset']}.",
                 conteudo=entrada['conteudo']
             )
         else:
@@ -99,6 +105,45 @@ class Lider(replicacao_pb2_grpc.ClienteServiceServicer):
                 mensagem="Chave não encontrada.",
                 conteudo=""
             )
+    
+    def salvar_em_log(self, entrada):
+        try:
+            path = f"trabalho5/logs/{self.arquivo_log}"
+            with open(path, "a", encoding="utf-8") as f:
+                linha = f"{entrada['epoca']},{entrada['offset']},{entrada['conteudo']}\n"
+                f.write(linha)
+        except Exception as e:
+            print(f"[Líder] Erro ao salvar log: {e}")
+
+    def salvar_em_banco(self, entrada):
+        try:
+            path = f"trabalho5/dados/{self.arquivo_banco}"
+            with open(path, "a", encoding="utf-8") as f:
+                linha = f"{entrada['epoca']},{entrada['offset']},{entrada['conteudo']}\n"
+                f.write(linha)
+        except Exception as e:
+            print(f"[Líder] Erro ao salvar banco: {e}")
+    
+    def carrega_banco(self):
+        with open(f"trabalho5/dados/{self.arquivo_banco}", "r", encoding="utf-8") as f:
+            for linha in f:
+                linha = linha.strip()
+                if not linha:
+                    continue
+                epoca_str, offset_str, conteudo = linha.split(",", 2)
+                epoca = int(epoca_str)
+                offset = int(offset_str)
+                chave = f"entrada_{offset}"
+                self.banco[chave] = {
+                    'epoca': epoca,
+                    'offset': offset,
+                    'conteudo': conteudo
+                }
+                if offset > self.offset:
+                    self.offset = offset
+
+                if epoca > self.epoca:
+                    self.epoca = epoca
         
 def main():
     replicas = ["localhost:50052", "localhost:50053", "localhost:50054"]
@@ -106,7 +151,7 @@ def main():
     lider = Lider(replicas)
     replicacao_pb2_grpc.add_ClienteServiceServicer_to_server(lider, servidor)
     servidor.add_insecure_port(f"[::]:{PORTA}")
-    print(f"Líder rodando na porta {PORTA}...")
+    print(f"Lider rodando na porta {PORTA}...")
     servidor.start()
     servidor.wait_for_termination()
 
--- trabalho5/replica.py
+++ trabalho5/replica.py
@@ -4,43 +4,96 @@ import replicacao_pb2, replicacao_pb2_grpc
 import argparse
 
 class Replica(replicacao_pb2_grpc.ServidorServiceServicer):
-    def __init__(self) -> None:
+    def __init__(self, porta) -> None:
         self.epoca = 1
-        self.offset = 0 
+        self.offset = 0
         self.log = []
-        self.banco: dict[str, dict] = {}
+        self.arquivo_banco = f"banco_replica_{porta}.txt"
+        self.arquivo_log = f"log_replica_{porta}.txt"
+        self.path = f"trabalho5/dados/{self.arquivo_banco}"
+        self.banco = {}
+
+        self.carrega_banco()
+
 
     def ReplicarEntrada(self, request, context):
-        print(f"[Replica] Recebida entrada epoca={request.epoca}, offset={request.offset}, conteudo='{request.conteudo}'")
+        print()
+        print(f"[Replica] Entrada recebida epoca={request.epoca} - offset={request.offset} - conteudo='{request.conteudo}'")
 
-        if request.epoca < self.epoca or (request.epoca == self and request.offset != len(self.log) + 1):
-            print(f"[Replica] Inconsistência detectada. Esperava offset {len(self.log)+1}, recebeu {request.offset}")
+        if request.epoca < self.epoca or (request.epoca == self.epoca and request.offset != self.offset + 1):
+            print(f"[Replica] Inconsistencia detectada. Esperava offset {self.offset + 1} e recebeu {request.offset}")
             self.truncar_log(request.offset)
             return replicacao_pb2.Ack(
                 sucesso=False,
-                mensagem="Inconsistência no log. Estado da réplica desatualizado."
+                mensagem="Inconsistencia no log. Estado da replica desatualizado."
             )
 
         self.log.append(request)
-        print(f"[Replica] Entrada armazenada no log intermediário.")
+        self.salvar_em_log(request)
+        print(f"[Replica] Entrada armazenada no log intermediario.")
         return replicacao_pb2.Ack(sucesso=True, mensagem="Entrada replicada com sucesso.")
     
     def CommitEntrada(self, request, context):
         print(f"[Replica] Ordem de commit recebida para epoca={request.epoca}, offset={request.offset}")
         if request.epoca != self.epoca:
-            return replicacao_pb2.Ack(sucesso=False, mensagem="Época inválida para commit.")
+            return replicacao_pb2.Ack(sucesso=False, mensagem="Epoca invalida para commit.")
         
         for entrada in self.log:
             if entrada.offset == request.offset:
-                self.banco[entrada.offset] = entrada.conteudo
+                chave = f"entrada_{entrada.offset}"
+                self.banco[chave] = {
+                    'epoca': entrada.epoca,
+                    'offset': entrada.offset,
+                    'conteudo': entrada.conteudo
+                }
+                self.salvar_em_arquivo(entrada)
+                self.offset += 1
                 print(f"[Replica] Entrada offset={entrada.offset} committed com sucesso.")
                 return replicacao_pb2.Ack(sucesso=True, mensagem="Commit efetuado.")
         
-        return replicacao_pb2.Ack(sucesso=False, mensagem="Offset não encontrado no log intermediário.")
+        return replicacao_pb2.Ack(sucesso=False, mensagem="Offset não encontrado no log intermediario.")
     
     def truncar_log(self, offset):
-        self.log = [entrada for entrada in self.log if entrada.offset < offset]
-        print(f"[Replica] Log truncado a partir do offset {offset}")
+        pass
+
+    def salvar_em_arquivo(self, entrada):
+        try:
+            with open(self.path, "a", encoding="utf-8") as f:
+                linha = f"{entrada.epoca},{entrada.offset},{entrada.conteudo}\n"
+                f.write(linha)
+        except Exception as e:
+            print(f"[Replica] Erro ao salvar em arquivo: {e}")
+
+    def salvar_em_log(self, entrada):
+        try:
+            path = f"trabalho5/logs/{self.arquivo_log}"
+            with open(path, "a", encoding="utf-8") as f:
+                linha = f"{entrada.epoca},{entrada.offset},{entrada.conteudo}\n"
+                f.write(linha)
+        except Exception as e:
+            print(f"[Replica] Erro ao salvar log intermediário: {e}")
+
+    def carrega_banco(self):
+        with open(self.path, "r", encoding="utf-8") as f:
+            for linha in f:
+                linha = linha.strip()
+                if not linha:
+                    continue
+                epoca_str, offset_str, conteudo = linha.split(",", 2)
+                epoca = int(epoca_str)
+                offset = int(offset_str)
+                chave = f"entrada_{offset}"
+                self.banco[chave] = {
+                    'epoca': epoca,
+                    'offset': offset,
+                    'conteudo': conteudo
+                }
+                if offset > self.offset:
+                    self.offset = offset
+
+                if epoca > self.epoca:
+                    self.epoca = epoca
+
 
 def main():
     parser = argparse.ArgumentParser(description="replica server")
@@ -48,7 +101,7 @@ def main():
     args = parser.parse_args()
 
     servidor = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    replicacao_pb2_grpc.add_ServidorServiceServicer_to_server(Replica(), servidor)
+    replicacao_pb2_grpc.add_ServidorServiceServicer_to_server(Replica(args.porta), servidor)
     servidor.add_insecure_port(f"[::]:{args.porta}")
     print(f"Réplica rodando na porta {args.porta}...")
     servidor.start()
